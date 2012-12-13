823d65cbc2de6b9b40e80b3dc150d9aa2997cecf
Nigel Small <nigel@nigelsmall.name>
2012-12-13 11:13:16 +0000

    added html function

--- src/syntaq/__init__.py
+++ src/syntaq/__init__.py
@@ -168,6 +168,11 @@ DOCUMENT_TEMPLATE="""\
 
 URL = re.compile(r"""(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""")
 
+def html(object):
+    try:
+        return object.__html__()
+    except AttributeError:
+        raise ValueError("Object cannot be expressed in HTML")
 
 class HTML(object):
 
